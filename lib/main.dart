65fcc8cc2da405a58166048905b2eb5823bb6fc2
amitmac <osm.amit29@gmail.com>
2025-02-07 12:29:44 +0530

    updated with latest flutter & dart versions.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:hrms_project/extras/Constants.dart';
 import 'package:hrms_project/screens/LoginScreen.dart';
 import 'package:hrms_project/screens/NavigatorScreen.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
@@ -22,20 +21,6 @@ Future<void> main() async {
         primary: const Color.fromRGBO(5, 42, 61, 1),
         secondary: const Color.fromRGBO(4, 46, 62, 0.10), // Secondary color
       ),
-    ),
-    builder: (context, child) => ResponsiveWrapper.builder(
-        BouncingScrollWrapper.builder(context, child!),
-        maxWidth: 1200,
-        minWidth: 450,
-        defaultScale: true,
-        breakpoints: [
-          const ResponsiveBreakpoint.resize(450, name: MOBILE),
-          const ResponsiveBreakpoint.autoScale(800, name: TABLET),
-          const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
-          const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
-          const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
-        ],
-        background: Container(color: const Color(0xFFF5F5F5))
     ),
     routes: <String, WidgetBuilder>{
       '/LoginScreen': (BuildContext context) => LoginScreen(),
